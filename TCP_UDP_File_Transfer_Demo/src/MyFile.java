df9dfb639ff84cc5fe274006e8f39e4dd4ad5626
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-09-26 14:22:46 -0400

    made a server class

---
+++ TCP_UDP_File_Transfer_Demo/src/MyFile.java
@@ -0,0 +1,15 @@
+public class MyFile {
+    private int id;
+    private String name;
+    private byte[] data;
+    private String fileExtension;
+
+    public MyFile(int id, String name, byte[] data, String fileExtension) {
+        this.id = id;
+        this.name = name;
+        this.data = data;
+        this.fileExtension = fileExtension;
+    }
+
+    
+}
